d3e519fa7ff1f2e4c5f4d81e1d42ce1abf5fe5fc
user <user@example.com>
2023-04-15 13:26:40 +0300

    Emojis

--- UI/wwwroot/js/Chat/media.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    const pickerOptions = {};
-    const picker = new EmojiMart.Picker(pickerOptions);
-    $('#emojis').append(picker);
-
-    $('#mediaModal').on('show.bs.modal', function () {
-
-    });
-});
